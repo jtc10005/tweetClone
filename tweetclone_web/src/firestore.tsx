5e053694e4f5fabcda829c67d25a06d8dbef789b
jtc10005 <jtc10005@gmail.com>
2019-01-14 21:17:21 -0600

    add react-firestore

---
+++ tweetclone_web/src/firestore.tsx
@@ -0,0 +1,10 @@
+import app from '@firebase/app';
+
+export  const FirestoreConfig = app.initializeApp({
+    apiKey: 'AIzaSyAhQQOK6w37uzrTdec00xmaJSc4Z-mzdz0',
+    authDomain: 'tweetclone-13474.firebaseapp.com',
+    databaseURL: 'https://tweetclone-13474.firebaseio.com',
+    projectId: 'tweetclone-13474',
+    storageBucket: 'tweetclone-13474.appspot.com',
+    messagingSenderId: '632970944995'
+});
